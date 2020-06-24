03989cb32dbae7f7c2053b1c5174de0769117f9c
chris-fleischer <chris-fleischer@hotmail.com>
2020-06-24 15:52:06 +0200

    extract data on wind and solar installations from OPSD

--- euses/resources.py
+++ euses/resources.py
@@ -15,7 +15,7 @@ class Power_Plants():
         year = nuts_2.year
         time_range = nuts_2.ds.time.values
 
-        df = pd.read_csv('data/resource/gppd/global_power_plant_database.csv')
+        df = pd.read_csv('data/resource/globalpowerplantdatabasev120/global_power_plant_database.csv')
         geometry = [Point(xy) for xy in zip(df.longitude, df.latitude)]
         df = df.drop(['longitude', 'latitude'], axis=1)
         crs = {'init': 'epsg:4326'}
@@ -25,7 +25,7 @@ class Power_Plants():
         df = pd.DataFrame()
         for nuts_2_geo in nuts_2.ds['geometry']:
             nuts_2_id = nuts_2_geo.coords['nuts_2'].values.item()
-            df1 = pd.DataFrame(gppd.loc[gppd.within(nuts_2_geo.item())].groupby('fuel1').sum().capacity_mw.rename(nuts_2_id)).T
+            df1 = pd.DataFrame(gppd.loc[gppd.within(nuts_2_geo.item())].groupby('primary_fuel').sum().capacity_mw.rename(nuts_2_id)).T
             df = df.append(df1)
         df = df.fillna(0)
 
@@ -44,7 +44,31 @@ class Power_Plants():
             ds = ds.drop('power_plants')
             ds = ds.drop('tech')
             ds['power_plants'] = df_1.T.to_xarray()['power_plants']
+
+        # Add solar and wind capacities from opsd data
+
+        df = pd.read_csv('data/resource/opsd/renewable_power_plants_EU.csv')
+        geometry = [Point(xy) for xy in zip(df.lon, df.lat)]
+        crs = {'init': 'epsg:4326'}
+        opsd_re = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
+        opsd_re = opsd_re.to_crs({'init': 'epsg:3035'})
+
+
+        for id in opsd_re.country.unique():
+            if id in ds.coords['nuts_0'].values:
+                dsc = ds.where(ds['country_code'] == id, drop=True)
+                opsd_re_c = opsd_re.loc[opsd_re.country ==id ]
+                df = pd.DataFrame()
+                for nuts_2_geo in dsc['geometry']:
+                    nuts_2_id = nuts_2_geo.coords['nuts_2'].values.item()
+                    df1 = pd.DataFrame(opsd_re_c.loc[opsd_re_c.within(nuts_2_geo.item())].groupby('energy_source_level_2').sum().electrical_capacity.rename(nuts_2_id)).T
+                    df1 = df1.fillna(0)
+                    for tech in ['Wind','Solar']:
+                        if tech in df1.columns:
+                            ds['power_plants'].loc[nuts_2_id,tech] = df1.loc[nuts_2_id,tech]
+
         nuts_2.ds = ds
+
 
 class Area():
     def __init__(self, nuts_2, scenario='Holtinger2016_med'):
