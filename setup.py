3dd9d74ea32e24e76eea54544734babb4399144a
chrisflei <christian.fleischer@uni-flensburg.de>
2021-02-18 12:12:21 +0100

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='euses',
+    version='1.0',
+    author='Christian Fleischer',
+    url='https://github.com/ENSYSTRA/EU-SES',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[]
+)
