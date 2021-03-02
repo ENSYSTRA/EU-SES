6067b7f1ccbf0626b6cf923cdbd678cffc6165c8
chrisflei <christian.fleischer@uni-flensburg.de>
2021-03-02 18:56:56 +0100

    Update manuscript_examples.py

--- examples/manuscript_examples.py
+++ examples/manuscript_examples.py
@@ -113,3 +113,81 @@ plt.text(-0.16, 1.7, '(a)',fontsize=13, transform=f3_ax1.transAxes, fontweight='
 plt.text(-0.16, 0.4, '(b)',fontsize=13, transform=f3_ax1.transAxes, fontweight='semibold')
 
 fig3.savefig('examples/example_figure.pdf',bbox_inches='tight')
+
+
+# Plot difference in installed capacity with and without pv in nuts 1 model
+results_dic={'nuts1_pv':'de_nuts1-pv-calliope-3h.nc','nuts1_nopv':'de_nuts1-nopv-calliope-3h.nc'}
+
+pf_difference = pd.DataFrame()
+pf_nopv = pd.DataFrame()
+pf_pv = pd.DataFrame()
+
+for region in ['region_{}'.format(r) for r in range(16)]:
+    pf = pd.DataFrame()
+    for name, dic in results_dic.items():
+        model = calliope.read_netcdf('calliope_model/results/{}'.format(dic))
+        pf[name] = model.get_formatted_array('energy_cap').loc[{'locs':region}].to_pandas()#.sum(axis=0).to_pandas()
+        ac_index = pf[name].filter(regex='ac_transmission:', axis=0).index
+        pf.loc['ac_transmission',name] = pf.loc[ac_index,name].sum()
+
+    ac_index = pf[name].filter(regex='ac_transmission:', axis=0).index
+    pf_clean = pf.drop(ac_index, axis=0)
+    pf_clean = pf_clean.drop(['demand_heat','demand_electricity','supply_biogas','supply_gas',], axis=0)
+    pf_clean = pf_clean.drop(['hphs','hror','hdam'], axis=0)
+    pf_nopv[region] = pf_clean['nuts1_nopv']
+    pf_pv[region] = pf_clean['nuts1_pv']
+pf_pv = pf_pv.T
+pf_nopv = pf_nopv.T
+pf_difference = (pf_nopv -pf_pv)/pf_pv *100
+
+state_names = ['Baden-Württemberg','Bayern','Niedersachsen','Nordrhein-Westfalen','Berlin',
+'Brandenburg','Bremen','Hamburg','Hessen','Mecklenburg-Vorpommern','Rheinland-Pfalz',
+'Saarland','Sachsen','Sachsen-Anhalt','Schleswig-Holstein','Thüringen']
+pf_difference['state_name'] = state_names
+
+regions = pf_difference.index.to_list()
+
+fig4, axs = plt.subplots(4,4, figsize=(15,20))
+for i in range(4):
+    for j in range(4):
+        region_pf = pf_difference.drop(['state_name','ac_transmission'],axis=1).filter(items=[regions[0]],axis=0)
+        state_name = pf_difference.loc[regions[0],'state_name']
+        techs = region_pf.columns.to_list()
+        colors = model.inputs['colors'].loc[techs].values
+        region_pf.plot.bar(title=state_name, ax=axs[i,j],color=colors)
+        axs[i,j].set_xticklabels([])
+        axs[i,j].set_xticks([])
+        if i ==3:
+            axs[i,j].set_xlabel("Technologies", fontsize=12)
+        if j ==0:
+            axs[i,j].set_ylabel('Percentage difference in installed capacity of \n GER NUTS1 without PV in reference to \n GER NUTS1 model with rooftop PV [%]')
+        legend = axs[i,j].get_legend()
+        legend.remove()
+        regions.remove(regions[0])
+        techs = pf_difference.drop(['state_name'],axis=1).columns.to_list()
+names =  model.inputs['names'].loc[techs].values
+plt.legend(labels = names, bbox_to_anchor=(0.8,-0.2), ncol=7)
+fig4.savefig('examples/capacity_difference-all_states.pdf',bbox_inches='tight')
+
+pf_difference = pf_difference.replace([np.inf, -np.inf], np.nan).fillna(0)
+regions_pf = pf_difference.loc[pf_difference.state_name.isin(['Berlin','Mecklenburg-Vorpommern'])]
+regions_pf.loc['Germany',:-1] = (pf_nopv.sum()-pf_pv.sum())/pf_pv.sum()*100
+regions_pf.loc['Germany','state_name'] = 'Germany'
+region_pf = regions_pf.drop(['ac_transmission'],axis=1)
+techs = region_pf.drop(['state_name'],axis=1).columns.to_list()
+colors = model.inputs['colors'].loc[techs].values
+names =  model.inputs['names'].loc[techs].values
+
+fig5, axs = plt.subplots(1,3, figsize=(10,4))
+
+for e,(region, rows) in enumerate(region_pf.iterrows()):
+    region_pf.filter(items=[region],axis=0).plot.bar(title=rows.state_name, ax=axs[e],color=colors)
+    axs[e].get_legend().remove()
+    axs[e].set_ylim(-80,10)
+    axs[e].set_xticklabels([])
+    axs[e].set_xticks([])
+    axs[e].set_xlabel("Technologies", fontsize=12)
+    axs[0].set_ylabel('Percentage difference in installed capacity of \n GER NUTS1 without PV in reference to \n GER NUTS1 model with rooftop PV [%]')
+plt.legend(labels = names, bbox_to_anchor=(1,-0.1), ncol=4)
+plt.tight_layout()
+fig5.savefig('examples/capacity_difference-selected_states.pdf',bbox_inches='tight')
