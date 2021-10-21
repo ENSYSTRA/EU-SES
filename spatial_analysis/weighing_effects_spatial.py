e9856f7f1a363a0383bda33f8d50383dadc3de9d
chrisflei <christian.fleischer@uni-flensburg.de>
2021-10-21 16:27:05 +0200

    add optimisation results

--- spatial_analysis/weighing_effects_spatial.py
+++ spatial_analysis/weighing_effects_spatial.py
@@ -11,24 +11,9 @@ import numpy as np
 import os, sys
 # sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-def run_scenario(countries, regions_method, area_factor, rooftop_pv, save_dir, national=False, var_weigthing='preset'):
-    calliope.set_log_verbosity('CRITICAL', include_solver_output=False)
-    eu_ds = euses.import_dataset('euses_datasets.nc')
-    # remove power plants not considered
-    fuels_considered = ['Biomass and biogas','Natural gas','Solar','Wind']
-    fuels_removed = np.setdiff1d(eu_ds.ds.coords['fuel'].values,fuels_considered)
-    eu_ds.ds = eu_ds.ds.drop(fuels_removed,dim='fuel')
-    tech_not_grouped = ['Solar','Wind','Wind Offshore']
-    tech_list = np.setdiff1d(eu_ds.ds.coords['tech'].values,tech_not_grouped)
-    # group natural gas and biomass/biogas based power_plants in single group
-    sum_var = eu_ds.ds['power_plants'].loc[:,tech_list,:].sum(axis=1)
-    tech_list = np.setdiff1d(tech_list,['Combined cycle'])
-    eu_ds.ds['power_plants'].loc[:,['Combined cycle']] = [[g] for g in sum_var.values]
-    eu_ds.ds = eu_ds.ds.drop(tech_list,dim='tech')
-    # only percentage of rooftop_pv and utility_pv area considered
-    eu_ds.ds['rooftop_pv'] = eu_ds.ds['rooftop_pv']*rooftop_pv
-    eu_ds.ds['utility_pv'] = eu_ds.ds['utility_pv']*0.50
-    filt_ds = eu_ds.filter_countries(countries)
+def run_scenario(euses_ds, countries, regions_method, area_factor=None, national=False, var_weigthing='preset'):
+    euses_ds.ds.load()
+    filt_ds = euses_ds.filter_countries(countries)
     # Create region suing var_weigthing setting
     if var_weigthing == 'preset':
         filt_ds.create_regions(regions_method, area_factor)
@@ -36,48 +21,18 @@ def run_scenario(countries, regions_method, area_factor, rooftop_pv, save_dir, n
         filt_ds.ds['area_pv'] = filt_ds.ds['rooftop_pv'] + filt_ds.ds['utility_pv']
         filt_ds.create_regions(regions_method, area_factor, var_weigthing = var_weigthing)
     # Build and solve calliope model
-    # filt_ds.create_regions(regions_method, area_factor)
     regions_gpd = gpd.GeoDataFrame(filt_ds.ds_regions['regions'].values,
     columns=['id'],geometry=filt_ds.ds_regions['geometry'].values)
     if national== True:
         regions_gpd['id'] = filt_ds.ds_regions['country_code'].values
     regions_gpd['area'] = [a.area for a in filt_ds.ds_regions['geometry'].values]
-    regions_gpd['heat'] = filt_ds.ds_regions['heat'].sum(axis=1).values
     regions_gpd['power'] = filt_ds.ds_regions['power'].sum(axis=1).values
     regions_gpd['pv_cf'] = filt_ds.ds_regions['pv_cf'].sum(axis=1).values
     regions_gpd['wind_cf'] = filt_ds.ds_regions['wind_cf'].sum(axis=1).values
-    regions_gpd['hydro_inflow'] = filt_ds.ds_regions['hydro_inflow'].sum(axis=1).values
-    # filt_ds.create_calliope_model(op_mode='plan',sectors=['power','heat'],co2_cap_factor=0.2, national=national)
-    # model = calliope.Model('calliope_model/model.yaml',scenario='time_3H',override_dict={'run.solver': 'gurobi'})
-    # model.run()
-    # model.to_netcdf('calliope_model/results/'+save_dir+'.nc')
+    regions_gpd['power'] = filt_ds.ds_regions['power'].sum(axis=1).values
+
     return regions_gpd
-    # return model, regions_gpd
 
-#--------------------------------------------------------------------------#
-# EU model (EU-nuts0)
-countries = ['Germany','Norway','Denmark','Poland','France','Netherlands',
-                                 'Belgium','Austria','Switzerland','Czech Rep.']
-
-scn_ref_eu_gpd = run_scenario(countries, 'poli_regions',None, 1, 'scn_ref_eu', national=True)
-
-var_weigthing_solar = {'wind_cf':'area','pv_cf':'area_pv','wind_offshore_cf':'offshore_wind',
-                        'cop_air':'area','hydro_inflow':'area'}
-scn_solar_eu_gpd = run_scenario(countries, 'poli_regions',None, 1,'scn_solar_eu', national=True,var_weigthing=var_weigthing_solar)
-
-var_weigthing_onshore_wind = {'wind_cf':'onshore_wind','pv_cf':'area','wind_offshore_cf':'offshore_wind',
-                        'cop_air':'area','hydro_inflow':'area'}
-scn_onshore_wind_eu_gpd = run_scenario(countries, 'poli_regions',None, 1,
-                                                'scn_onshore_wind_eu', national=True,var_weigthing=var_weigthing_onshore_wind)
-
-gpd_ds = scn_ref_eu_gpd[['id','geometry']]
-gpd_ds['wind_onshore_percentage_change'] = (scn_onshore_wind_eu_gpd['wind_cf'] - scn_ref_eu_gpd['wind_cf'])/ scn_ref_eu_gpd['wind_cf']*100
-gpd_ds['solar_pv_percentage_change'] = (scn_solar_eu_gpd['pv_cf'] - scn_ref_eu_gpd['pv_cf'])/ scn_ref_eu_gpd['pv_cf']*100
-scn_ref_eu_gpd[['pv_cf','id']]
-
-scn_solar_eu_gpd[['pv_cf','id']]
-import matplotlib
-import matplotlib.pyplot as plt
 def cmap_map(function, cmap):
     """ Applies function (which should operate on vectors of shape 3: [r, g, b]), on colormap cmap.
     This routine will break any discontinuous points in a colormap.
@@ -109,49 +64,243 @@ def cmap_map(function, cmap):
     return matplotlib.colors.LinearSegmentedColormap('colormap',cdict,1024)
 
 
-lgk = {"fmt": "{:.0f}", 'loc': 'upper left'}
-light_jet = cmap_map(lambda x: x/2 + 0.5, matplotlib.cm.jet)
+eu_ds = euses.import_dataset('euses_ds_2011_v1.nc')
 
-plots = {'solar_pv_percentage_change':'Solar PV CF percentage change',
-        'wind_onshore_percentage_change':'Onshore wind percentage change',}
-        # 'hydro_inflow_percentage_change':'Hydro mean inflow percentage change',}
+# var_weigthing_solar = {'wind_cf':'area','pv_cf':'area_pv','wind_offshore_cf':'offshore_wind',
+#                         'cop_air':'area','hydro_inflow':'area'}
+# var_weigthing_onshore_wind = {'wind_cf':'onshore_wind','pv_cf':'area','wind_offshore_cf':'offshore_wind',
+#                         'cop_air':'area','hydro_inflow':'area'}
+#
+#
+# gpd_pc = gpd.GeoDataFrame()
+# from euses.parameters import countries
+#
+# for c in countries[:1]:
+#     id = c.get('nuts_id')
+#     id_ds = id
+#     if id == 'GR':
+#         id_ds = 'EL'
+#
+#     name = [c.get('name')]
+#     if name not in ['Estonia','Latvia','Lithuania','Luxembourg','Norway']:
+#         scn_ref = run_scenario(eu_ds, name, 'poli_regions_nuts1',)
+#         scn_solar = run_scenario(eu_ds,name, 'poli_regions_nuts1',var_weigthing=var_weigthing_solar)
+#         scn_onshore_wind = run_scenario(eu_ds,name, 'poli_regions_nuts1',var_weigthing=var_weigthing_onshore_wind)
+#
+#         gpd_pc_c = scn_ref[['id','geometry']]
+#         gpd_pc_c['wind_onshore_pc'] = (scn_onshore_wind['wind_cf'] - scn_ref['wind_cf'])/ scn_ref['wind_cf']*100
+#         gpd_pc_c['solar_pv_pc'] = (scn_solar['pv_cf'] - scn_ref['pv_cf'])/ scn_ref['pv_cf']*100
+#         gpd_pc = pd.concat([gpd_pc_c,gpd_pc])
+# gpd_pc[['id','solar_pv_pc']].max()
+#
+# de_ds_pop = eu_ds.filter_countries(['Germany'])
+#
+# de_ds = eu_ds.filter_countries(['Germany'])
+# ds = de_ds.ds
+# gdp_eu = pd.read_excel('https://knowledge4policy.ec.europa.eu/sites/default/files/RUVGD.ods')
+# gdp_de = gdp_eu.loc[gdp_eu.NUTS_ID.isin(ds.coords['nuts_2'].values)]
+# gdp_de_eur = gdp_de.loc[gdp_de.Unit=='Million EUR']
+# gdp_de_eur.index = gdp_de_eur.NUTS_ID
+#
+#
+# de_ds.ds['gdp'] = (('nuts_2'),(np.array(gdp_de_eur[2015])))
+# de_ds.ds['gdp'].attrs['unit'] = 'Mio EUR (ref. 2015)'
+#
+# de_ds.add('Power',disaggregation_var='gdp')
+#
+# scn_pop = run_scenario(de_ds_pop,['Germany'], 'poli_regions_nuts1')
+# scn_gdp = run_scenario(de_ds,['Germany'], 'poli_regions_nuts1')
+#
+# scn_pop = gpd.GeoDataFrame(de_ds_pop.ds['nuts_2'].values,
+# columns=['id'],geometry=de_ds_pop.ds['geometry'].values)
+# scn_pop['power'] = de_ds_pop.ds['power'].sum(axis=1).values
+#
+# scn_gdp = gpd.GeoDataFrame(de_ds.ds['nuts_2'].values,
+# columns=['id'],geometry=de_ds.ds['geometry'].values)
+# scn_gdp['power'] = de_ds.ds['power'].sum(axis=1).values
+#
+# gpd_pc = scn_pop
+# gpd_pc['power'] = (scn_pop[['power']]-scn_gdp[['power']])/scn_gdp[['power']]*100
+# gpd_pc.plot(column = 'power')
+#
+#
+# lgk = {"fmt": "{:.0f}", 'loc': 'upper left'}
+# light_jet = cmap_map(lambda x: x/2 + 0.5, matplotlib.cm.jet)
+# fig, ax = plt.subplots(figsize=(12,15))
+# bins_off = {'bins':[-300,-100,0,100,300]}
+# gpd_pc.plot(column=p, legend=True, scheme='user_defined',
+#             classification_kwds = bins_off,
+#             cmap = light_jet,ax=ax,
+#             figsize=(15, 10), legend_kwds = lgk)
+# for x, y, label in zip(gpd_ds.geometry.centroid.x, gpd_ds.geometry.centroid.y, gpd_pc[p]):
+#     ax.annotate(round(label,1), xy=(x, y), xytext=(3, 3), textcoords="offset points")
+#
+# # new_label=['<-5', '-5 to -2','-2 to 1','-1 to 0','0 to 1','>1']
+# new_label=['<-5', '-5 to -1','-1 to 0','0 to 1','1 to 5','>5']
+#
+# ax.set_title(title)
+#
+# max_val = round(gpd_ds[p].max(),-2)
+# ax.set_axis_off()
+# leg = ax.get_legend()
+# for text, label in zip(leg.get_texts(), new_label):
+#     text.set_text(label)
+#
+#
+#
+#
+#
+# gpd_ds = scn_ref_eu_gpd[['id','geometry']]
+# gpd_ds['wind_onshore_percentage_change'] = (scn_onshore_wind_eu_gpd['wind_cf'] - scn_ref_eu_gpd['wind_cf'])/ scn_ref_eu_gpd['wind_cf']*100
+# gpd_ds['solar_pv_percentage_change'] = (scn_solar_eu_gpd['pv_cf'] - scn_ref_eu_gpd['pv_cf'])/ scn_ref_eu_gpd['pv_cf']*100
+# scn_ref_eu_gpd[['pv_cf','id']]
+#
+# scn_solar_eu_gpd[['pv_cf','id']]
+# import matplotlib
+# import matplotlib.pyplot as plt
+#
+#
+# lgk = {"fmt": "{:.0f}", 'loc': 'upper left'}
+# light_jet = cmap_map(lambda x: x/2 + 0.5, matplotlib.cm.jet)
+#
+# plots = {'solar_pv_percentage_change':'Solar PV CF percentage change',
+#         'wind_onshore_percentage_change':'Onshore wind percentage change',}
+#         # 'hydro_inflow_percentage_change':'Hydro mean inflow percentage change',}
+#
+# for p, title in plots.items():
+#     fig, ax = plt.subplots(figsize=(12,15))
+#     bins_off = {'bins':[-5,-1,0,1,5]}
+#     gpd_ds.plot(column=p, legend=True, scheme='user_defined',
+#                 classification_kwds = bins_off,
+#                 cmap = light_jet,ax=ax,
+#                 figsize=(15, 10), legend_kwds = lgk)
+#     for x, y, label in zip(gpd_ds.geometry.centroid.x, gpd_ds.geometry.centroid.y, gpd_ds[p]):
+#         ax.annotate(round(label,1), xy=(x, y), xytext=(3, 3), textcoords="offset points")
+#
+#     # new_label=['<-5', '-5 to -2','-2 to 1','-1 to 0','0 to 1','>1']
+#     new_label=['<-5', '-5 to -1','-1 to 0','0 to 1','1 to 5','>5']
+#
+#     ax.set_title(title)
+#
+#     max_val = round(gpd_ds[p].max(),-2)
+#     ax.set_axis_off()
+#     leg = ax.get_legend()
+#     for text, label in zip(leg.get_texts(), new_label):
+#         text.set_text(label)
+#
+#     # fig.savefig('spatial_analysis/{}.pdf'.format(p))
+#
+#
 
-for p, title in plots.items():
-    fig, ax = plt.subplots(figsize=(12,15))
-    bins_off = {'bins':[-5,-1,0,1,5]}
-    gpd_ds.plot(column=p, legend=True, scheme='user_defined',
-                classification_kwds = bins_off,
-                cmap = light_jet,ax=ax,
-                figsize=(15, 10), legend_kwds = lgk)
-    for x, y, label in zip(gpd_ds.geometry.centroid.x, gpd_ds.geometry.centroid.y, gpd_ds[p]):
-        ax.annotate(round(label,1), xy=(x, y), xytext=(3, 3), textcoords="offset points")
 
-    # new_label=['<-5', '-5 to -2','-2 to 1','-1 to 0','0 to 1','>1']
-    new_label=['<-5', '-5 to -1','-1 to 0','0 to 1','1 to 5','>5']
-
-    ax.set_title(title)
-
-    max_val = round(gpd_ds[p].max(),-2)
-    ax.set_axis_off()
-    leg = ax.get_legend()
-    for text, label in zip(leg.get_texts(), new_label):
-        text.set_text(label)
-
-    # fig.savefig('spatial_analysis/{}.pdf'.format(p))
-
-
-scn_ref_eu_model = calliope.read_netcdf('calliope_model/results/scn_ref_eu.nc')
-scn_solar_eu_model = calliope.read_netcdf('calliope_model/results/scn_solar_eu.nc')
-# scn_onshore_wind_eu_model = calliope.read_netcdf('calliope_model/results/scn_onshore_wind_eu.nc')
-
+from euses.parameters import countries
+path = 'calliope_model/results/'
 techs= ['solar','wind', 'wind_offshore','combined_cycle', 'heat_pump_air','battery', 'hdam',
         'hphs', 'hror', 'hydrogen']
-df_IC = pd.DataFrame()
-fig_dic = {scn_ref_eu_model:'scn_ref_eu',scn_ref_eu_model:'scn_ref_eu',scn_solar_eu_model:'scn_solar_eu'}
+techs_eg = ['solar','wind', 'wind_offshore','combined_cycle', 'heat_pump_air', 'hdam', 'hror',]
 
-for model, name in fig_dic.items():
-    installed_capacity = model.get_formatted_array('energy_cap').loc['NO',:]
-    df_IC[name] = installed_capacity.loc[techs].to_pandas()
-    for transmission in ['ac_transmission','dc_transmission']:
-        df_IC.loc[transmission,name] = installed_capacity.loc[installed_capacity.coords['techs'].str.contains(transmission)].sum()
-df_IC=df_IC/1e6 # convert to TW from GW
+df_PC_solar = pd.DataFrame()
+df_PC_wind = pd.DataFrame()
+df_IC = pd.DataFrame()
+df_EG = pd.DataFrame()
+
+for c in countries:
+    id = c.get('nuts_id')
+    id_ds = id
+    if id == 'GR':
+        id_ds = 'EL'
+
+    name = c.get('name')
+    if name not in ['Estonia','Latvia','Lithuania','Luxembourg']:
+        techs= ['solar','wind', 'wind_offshore','combined_cycle', 'heat_pump_air','battery', 'hdam',
+                'hphs', 'hror', 'hydrogen']
+        for scn in ['scn_ref_','scn_solar_','scn_onshore_wind_']:
+            scn_name = scn + id
+            model = calliope.read_netcdf('{}{}/{}.nc'.format(path,name,scn_name))
+            # model.inputs['colors'].loc['dc_transmission'] = '#D2BB5F'
+            colors = model.inputs['colors'].loc[techs].values
+            names = model.inputs['names'].loc[techs].values
+            installed_capacity = model.get_formatted_array('energy_cap').loc[id_ds,:]
+            df_IC[scn_name] = installed_capacity.loc[techs].to_pandas()
+            # for transmission in ['ac_transmission','dc_transmission']:
+            #     df_IC.loc[transmission,scn+id] = installed_capacity.loc[installed_capacity.coords['techs'].str.contains(transmission)].sum()
+            energy_generated = model.get_formatted_array('carrier_prod').loc[['heat','power'],id_ds,techs_eg].sum(axis=2)
+            energy_consumed_hp = model.get_formatted_array('carrier_con').loc[['power'],id_ds,'heat_pump_air'].sum(axis=1)
+            df_EG[scn_name] = (energy_generated - energy_consumed_hp).sum(axis=0).to_pandas()
+
+        df_PC_solar[id] = (df_IC['scn_solar_'+id]-df_IC['scn_ref_'+id])/df_IC['scn_ref_'+id]*100
+        df_PC_wind[id] = (df_IC['scn_onshore_wind_'+id]-df_IC['scn_ref_'+id])/df_IC['scn_ref_'+id]*100
+
+col = df_EG.loc[:,df_EG.columns.str.contains('scn_ref_')].columns
+df_EG_ref = df_EG.loc[:,col]
+df_EG_ref.columns = [c[-2:] for c in df_EG_ref]
+
+fig, ax = plt.subplots()
+df_EG_ref.transpose().plot.bar(ax=ax,stacked=True, color=colors, legend=True)
+
+#convert figures from MW to GW
+df_IC = df_IC*1e-3
+
+col = df_IC.loc[:,df_IC.columns.str.contains('scn_ref_')].columns
+df_IC_ref = df_IC.loc[:,col]
+df_IC_ref.columns = [c[-2:] for c in df_IC_ref]
+
+fig, ax = plt.subplots()
+df_IC_ref.transpose().plot.bar(ax=ax,stacked=True, color=colors, legend=True)
+
+fig, ax = plt.subplots(figsize=(11,6))
+for e,tech in enumerate(df_PC_wind.index):
+    y_axis = df_PC_wind.loc[tech]
+    ax.scatter(x=df_PC_wind.columns,y=df_PC_wind.loc[tech],s=70,color=colors[e],alpha=0.8,label=names[e])
+    ax.set_yscale('symlog')
+    ax.legend(loc=(0,-0.25),ncol=5)
+    ax.set_ylim(-120,122)
+    plt.grid(True,linestyle='--')
+
+fig, ax = plt.subplots(figsize=(11,6))
+for e,tech in enumerate(df_PC_solar.index):
+    y_axis = df_PC_solar.loc[tech]
+    ax.scatter(x=df_PC_solar.columns,y=df_PC_solar.loc[tech],s=70,color=colors[e],alpha=0.7,label=names[e])
+    ax.set_yscale('symlog')
+    ax.legend(loc=(0,-0.25),ncol=5)
+    ax.set_ylim(-120,120)
+    plt.grid(True,linestyle='--')
+
+
+# model_fr = calliope.read_netcdf('{}{}/{}.nc'.format(path,'France','scn_ref_FR'))
+model_it = calliope.read_netcdf('{}{}/{}.nc'.format(path,'Italy','scn_ref_IT'))
+model_uk = calliope.read_netcdf('{}{}/{}.nc'.format(path,'Great Britain','scn_ref_UK'))
+
+# uk = eu_ds.filter_countries(['Great Britain'])
+# uk.ds['heat'].sum()
+
+con_pow = pd.DataFrame()
+pro_pow = pd.DataFrame()
+con_heat = pd.DataFrame()
+pro_heat = pd.DataFrame()
+
+con_pow['UK'] = model_uk.get_formatted_array('carrier_con').loc['power','UK',:].sum(axis=1).to_pandas()
+con_pow['IT'] = model_it.get_formatted_array('carrier_con').loc['power','IT',:].sum(axis=1).to_pandas()
+con_heat['UK'] = model_uk.get_formatted_array('carrier_con').loc['heat','UK',:].sum(axis=1).to_pandas()
+con_heat['IT'] = model_it.get_formatted_array('carrier_con').loc['heat','IT',:].sum(axis=1).to_pandas()
+
+pro_pow['UK'] = model_uk.get_formatted_array('carrier_prod').loc['power','UK',:].sum(axis=1).to_pandas()
+pro_pow['IT'] = model_it.get_formatted_array('carrier_prod').loc['power','IT',:].sum(axis=1).to_pandas()
+pro_heat['UK'] = model_uk.get_formatted_array('carrier_prod').loc['heat','UK',:].sum(axis=1).to_pandas()
+pro_heat['IT'] = model_it.get_formatted_array('carrier_prod').loc['heat','IT',:].sum(axis=1).to_pandas()
+
+model_uk.plot.timeseries(array=['heat', 'resource_con'])
+model_.plot.timeseries(array=['heat', 'resource_con'])
+
+
+model_uk.get_formatted_array('carrier_con').loc['heat',:,'demand_heat'].sum()
+#
+it_con = model_it.get_formatted_array('carrier_con').loc['power','UK',:].sum(axis=1).to_pandas()
+model_it.get_formatted_array('carrier_con').loc['heat',:,'demand_heat'].sum()
+
+col = df_EG.loc[:,df_EG.columns.str.contains('scn_ref_')].columns
+df_EG_ref = df_EG.loc[:,col]
+df_EG_ref.columns = [c[-2:] for c in df_EG_ref]
+
+fig, ax = plt.subplots()
+df_EG_ref.transpose().plot.bar(ax=ax,stacked=True, color=colors, legend=True)
--- spatial_analysis/weighting_effect.py
+++ spatial_analysis/weighting_effect.py
@@ -11,23 +11,9 @@ import numpy as np
 import os, sys
 # sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-def run_scenario(countries, regions_method, area_factor, rooftop_pv, save_dir, national=False, var_weigthing='preset'):
-    calliope.set_log_verbosity('CRITICAL', include_solver_output=False)
-    eu_ds = euses.import_dataset('euses_datasets.nc')
-    # remove power plants not considered
-    fuels_considered = ['Biomass and biogas','Natural gas','Solar','Wind']
-    fuels_removed = np.setdiff1d(eu_ds.ds.coords['fuel'].values,fuels_considered)
-    eu_ds.ds = eu_ds.ds.drop(fuels_removed,dim='fuel')
-    tech_not_grouped = ['Solar','Wind','Wind Offshore']
-    tech_list = np.setdiff1d(eu_ds.ds.coords['tech'].values,tech_not_grouped)
-    # group natural gas and biomass/biogas based power_plants in single group
-    sum_var = eu_ds.ds['power_plants'].loc[:,tech_list,:].sum(axis=1)
-    tech_list = np.setdiff1d(tech_list,['Combined cycle'])
-    eu_ds.ds['power_plants'].loc[:,['Combined cycle']] = [[g] for g in sum_var.values]
-    eu_ds.ds = eu_ds.ds.drop(tech_list,dim='tech')
-    # only percentage of rooftop_pv and utility_pv area considered
-    eu_ds.ds['rooftop_pv'] = eu_ds.ds['rooftop_pv']*rooftop_pv
-    eu_ds.ds['utility_pv'] = eu_ds.ds['utility_pv']*0.50
+
+
+def run_scenario(countries, regions_method, area_factor, save_dir, national=False, var_weigthing='preset'):
     filt_ds = eu_ds.filter_countries(countries)
     # Create region suing var_weigthing setting
     if var_weigthing == 'preset':
@@ -36,41 +22,64 @@ def run_scenario(countries, regions_method, area_factor, rooftop_pv, save_dir, n
         filt_ds.ds['area_pv'] = filt_ds.ds['rooftop_pv'] + filt_ds.ds['utility_pv']
         filt_ds.create_regions(regions_method, area_factor, var_weigthing = var_weigthing)
     # Build and solve calliope model
-    # filt_ds.create_regions(regions_method, area_factor)
-    regions_gpd = gpd.GeoDataFrame(filt_ds.ds_regions['regions'].values,
-    columns=['id'],geometry=filt_ds.ds_regions['geometry'].values)
     filt_ds.create_calliope_model(op_mode='plan',sectors=['power','heat'],co2_cap_factor=0.2, national=national)
     model = calliope.Model('calliope_model/model.yaml',scenario='time_3H',override_dict={'run.solver': 'gurobi'})
     model.run()
-    model.to_netcdf('calliope_model/results/'+save_dir+'.nc')
+    model.to_netcdf('calliope_model/results/North_Sea/'+save_dir+'.nc')
+    model,filt_ds = None, None
 
-    return model, regions_gpd
 
+calliope.set_log_verbosity('CRITICAL', include_solver_output=False)
+eu_ds = euses.import_dataset('euses_datasets.nc')
+# remove power plants not considered
+fuels_considered = ['Biomass and biogas','Natural gas','Solar','Wind']
+fuels_removed = np.setdiff1d(eu_ds.ds.coords['fuel'].values,fuels_considered)
+eu_ds.ds = eu_ds.ds.drop(fuels_removed,dim='fuel')
+tech_not_grouped = ['Solar','Wind','Wind Offshore']
+tech_list = np.setdiff1d(eu_ds.ds.coords['tech'].values,tech_not_grouped)
+# group natural gas and biomass/biogas based power_plants in single group
+sum_var = eu_ds.ds['power_plants'].loc[:,tech_list,:].sum(axis=1)
+tech_list = np.setdiff1d(tech_list,['Combined cycle'])
+eu_ds.ds['power_plants'].loc[:,['Combined cycle']] = [[g] for g in sum_var.values]
+eu_ds.ds = eu_ds.ds.drop(tech_list,dim='tech')
 #--------------------------------------------------------------------------#
 # EU model (EU-nuts0)
-countries = ['Germany','Norway','Denmark','Poland','France','Netherlands',
-                                 'Belgium','Austria','Switzerland','Czech Rep.']
+countries = ['Germany','Norway','Denmark','Sweden','Netherlands','Great Britain']
+
+
 var_weigthing_solar = {'wind_cf':'area','pv_cf':'area_pv','wind_offshore_cf':'offshore_wind',
 'cop_air':'area','hydro_inflow':'area'}
+
 var_weigthing_onshore_wind = {'wind_cf':'onshore_wind','pv_cf':'area','wind_offshore_cf':'offshore_wind',
 'cop_air':'area','hydro_inflow':'area'}
 
-scn_ref_eu_model, scn_ref_eu_gpd = run_scenario(countries, 'poli_regions',None, 1, 'scn_ref_eu', national=True)
+run_scenario(countries, 'poli_regions',None,'scn_ref_ns', national=True)
 
-scn_solar_eu_model, scn_solar_eu_gpd = run_scenario(countries, 'poli_regions',None, 1,
-                                                'scn_solar_eu', national=True,var_weigthing=var_weigthing_solar)
+run_scenario(countries, 'poli_regions',None,'scn_solar_ns', national=True,var_weigthing=var_weigthing_solar)
 
-scn_onshore_wind_eu_model, scn_onshore_wind_eu_gpd = run_scenario(countries, 'poli_regions',None, 1,
-                                                'scn_onshore_wind_eu', national=True,var_weigthing=var_weigthing_onshore_wind)
+run_scenario(countries, 'poli_regions',None,'scn_onshore_wind_ns', national=True,var_weigthing=var_weigthing_onshore_wind)
 
 # --------------------------------------------------------------------------#
 # Germany administrative regions model (GER-nuts1) with and without rooftop_pv
 countries = ['Germany']
-de_nuts1_model, de_nuts1_gpd = run_scenario(['Germany'], 'poli_regions_nuts1', None , 1, 'de_nuts1-pv-calliope-3h')
-de_nuts1_nopv_model, de_nuts1_gpd = run_scenario(['Germany'], 'poli_regions_nuts1', None , 0, 'de_nuts1-nopv-calliope-3h')
+scn_ref_de_model, scn_ref_de_gpd = run_scenario(countries, 'poli_regions_nuts1',None, 'scn_ref_de', national=False)
 
-scn_ref_de_model, scn_ref_de_gpd = run_scenario(countries, 'poli_regions_nuts1',None, 1, 'scn_ref_de', national=True)
+scn_solar_de_model, scn_solar_de_gpd = run_scenario(countries, 'poli_regions_nuts1',None, 'scn_solar_de', national=False,var_weigthing=var_weigthing_solar)
 
-scn_solar_de_model, scn_solar_de_gpd = run_scenario(countries, 'poli_regions_nuts1',None, 1, 'scn_solar_de', national=True,var_weigthing=var_weigthing_solar)
+scn_onshore_wind_de_model, scn_onshore_wind_eu_gpd = run_scenario(countries, 'poli_regions_nuts1',None,'scn_onshore_wind_de', national=False,var_weigthing=var_weigthing_onshore_wind)
 
-scn_onshore_wind_de_model, scn_onshore_wind_eu_gpd = run_scenario(countries, 'poli_regions_nuts1',None, 1,'scn_onshore_wind_de', national=True,var_weigthing=var_weigthing_onshore_wind)
+# --------------------------------------------------------------------------#
+# --------------------------------------------------------------------------#
+# --------------------------------------------------------------------------#
+# --------------------------------------------------------------------------#
+
+for loc in scn_ref_eu_model.inputs.coords['locs']:
+    loc
+    (scn_ref_eu_model.get_formatted_array('resource')/3).loc[loc:,'wind',:].to_pandas().transpose().plot()
+
+(scn_ref_eu_model.get_formatted_array('resource')/3).loc[:,'wind',:].to_pandas().transpose().plot()
+
+(scn_onshore_wind_eu_model.get_formatted_array('resource')/3).loc[:,'wind',:].to_pandas().transpose().plot()
+
+scn_solar_eu_model
+scn_onshore_wind_eu_model
